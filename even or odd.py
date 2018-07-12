d9f64c083c0cd48ce482179040ba4f1423abd288
balajisivakumar <40801671+Balajisivakumar92@users.noreply.github.com>
2018-07-12 21:17:24 +0530

    Add files via upload

---
+++ even or odd.py
@@ -0,0 +1,11 @@
+#ask the user for a number.depending on whether the number is even or odd
+#print out an appropriate message to the user.
+
+num = input('enter any number:')
+mod = num % 2
+
+if mod > 0:
+    print 'you have entered odd number:',num
+else:
+    print 'you have entered even number:',num
+    
