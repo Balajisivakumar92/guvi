47757693074021406c15cafdee12089480781ff4
balajisivakumar <40801671+Balajisivakumar92@users.noreply.github.com>
2018-07-12 21:44:53 +0530

    Add files via upload

---
+++ Check Alphabet or Not.py
@@ -0,0 +1,8 @@
+character = raw_input('enter any character :')
+
+if ((character>='a' and ch<='z') or (character>='A' and character<='Z')):
+	print (character,'is an alphabet.')
+else:
+	print (character,'is not an alphabet.')
+
+	
