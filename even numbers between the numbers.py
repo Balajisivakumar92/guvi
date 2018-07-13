a445f92c2c270397dcc9b2ac4b6b184464ed0616
balajisivakumar <40801671+Balajisivakumar92@users.noreply.github.com>
2018-07-13 16:21:51 +0530

    Add files via upload

---
+++ even numbers between the numbers.py
@@ -0,0 +1,8 @@
+x,z = input('enter a number :')
+a = range(x,z)
+b = a[:-1]
+c = a[1:]
+for i in c:
+    if i % 2 == 0:
+        print i
+		
---
+++ odd number between a numbers.py
@@ -0,0 +1,9 @@
+x,z = input('enter a number :')
+a = range(x,z)
+b = a[:-1]
+c = a[1:]
+for i in c:
+    if i % 2 == 1:
+        print i
+		
+
