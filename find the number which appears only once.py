30adc0f8442521c347dfa051d0e7e3ff819a9f4f
balajisivakumar <40801671+Balajisivakumar92@users.noreply.github.com>
2018-07-14 20:00:19 +0530

    Add files via upload

---
+++ find the number which appears only once.py
@@ -0,0 +1,9 @@
+import random
+import collections
+
+x = input('enter any number :')
+q = []
+for i in range(x):
+    q.append(random.randint(1, x))
+print q
+print [i for i, count in collections.Counter(q).items() if count <= 1]
